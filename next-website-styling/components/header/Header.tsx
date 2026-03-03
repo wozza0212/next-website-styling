b54f6813f4cfd6f176938b54d0bb8d1106dd8f50
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2026-03-03 21:34:54 +0000

    wip

---
+++ next-website-styling/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h1>This is the navbar</h1>
+    </div>
+  );
+};
+
+export default Header;
