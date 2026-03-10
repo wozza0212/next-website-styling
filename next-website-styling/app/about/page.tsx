4e98f5ccda99dd8966ec0d57159fd0f350e1b3e2
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2026-03-10 20:41:12 +0000

    add about page

---
+++ next-website-styling/app/about/page.tsx
@@ -0,0 +1,61 @@
+const About = () => {
+  return (
+    <div>
+      <main>
+        <div>
+          <h1> This is the About Page</h1>
+        </div>
+        <div>
+          <h3>Lorem Ipsum</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            sagittis ultrices turpis, sit amet rhoncus orci tristique vitae. Nam
+            venenatis massa vitae erat consectetur, ut euismod sem finibus.
+            Etiam nisi ante, aliquam eget mi non, vehicula maximus nisi. Fusce
+            maximus fringilla purus quis dignissim. Duis elementum lectus eu leo
+            consectetur, id finibus ex laoreet. Cras semper dignissim metus eu
+            semper. Morbi ultricies iaculis libero. Phasellus id sodales mauris,
+            vitae suscipit eros. Nunc mollis condimentum blandit. Aenean dictum
+            sagittis metus et ullamcorper. Vestibulum pellentesque ex nisl,
+            ornare pharetra lectus finibus in. Duis ullamcorper at enim non
+            vulputate. Vestibulum sed nibh erat. Nullam nec vestibulum tellus.
+            Praesent eu consequat metus. Cras ultricies mattis risus, non
+            egestas eros sollicitudin at. Nam sit amet mattis nibh. Suspendisse
+            eleifend aliquam ante sit amet tincidunt. Nunc fermentum tincidunt
+            quam. Cras eu laoreet metus. Duis eget arcu nulla. Duis iaculis
+            pulvinar dui tempor porta. Morbi quis turpis ultrices, sodales enim
+            vel, consectetur elit. Proin ut mi a lectus porta cursus eget id
+            turpis. In ultricies blandit finibus. Aliquam lacus elit, vehicula
+            sed ipsum in, venenatis mollis justo. Curabitur interdum non ligula
+            at vehicula. Ut lobortis, lacus quis porttitor tempor, justo erat
+            ultricies mi, id tempus lacus urna suscipit odio. Vivamus vel
+            aliquam ipsum, eget pretium nulla. Sed eget malesuada justo, nec
+            bibendum turpis. Ut et feugiat quam. Mauris scelerisque risus sed
+            rhoncus sagittis. Aliquam porttitor dui ac nulla placerat mollis in
+            at ante. Nulla nec ligula vitae magna tincidunt pellentesque. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel ornare
+            erat, vitae dictum enim. Maecenas ligula nisl, ullamcorper viverra
+            ante in, finibus pharetra libero. Donec mattis dui neque, eget
+            commodo arcu fermentum sed. Ut sodales lectus in turpis euismod, ac
+            hendrerit sem auctor. Curabitur consequat eleifend magna quis
+            laoreet. Nunc orci metus, faucibus at ultricies vitae, viverra sit
+            amet nisl. Fusce risus dui, molestie a pretium ut, laoreet at felis.
+            Pellentesque vehicula nibh et lorem accumsan, ut vehicula magna
+            auctor. Sed laoreet, tellus non dapibus congue, leo sem gravida
+            tellus, in pellentesque mauris tellus vitae ex. Sed ultricies erat
+            ac posuere sodales. Class aptent taciti sociosqu ad litora torquent
+            per conubia nostra, per inceptos himenaeos. Integer porta id ante in
+            pharetra. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Maecenas augue mauris, semper sit amet justo vel, dictum mattis
+            sapien. Duis accumsan euismod metus eu pharetra. Morbi in purus non
+            nisi dapibus varius. Etiam tempus condimentum justo, egestas rutrum
+            velit molestie id. Aliquam condimentum elementum neque, quis rhoncus
+            velit posuere ultricies.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default About;
