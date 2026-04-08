6df29d3dcee3b616304a4d18348b4d7d96c53d93
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2026-04-08 21:18:00 +0100

    small update

--- next-website-styling/components/Hero/Hero.tsx
+++ next-website-styling/components/Hero/Hero.tsx
@@ -2,26 +2,28 @@ import Image from "next/image";
 import styles from "./Hero.module.css";
 const HeroSection = () => {
   return (
-    <section className={styles.hero}>
-      <div className={styles.heroText}>
-        <p>
-          Hey there, I'm Tom, my friends call me Wozza or Tommo though. I'm
-          currently employed as a software engineer, so I thought I'd build this
-          site in my spare time ot help me develop my skills and help me feel
-          more like a software engineer. Maybe help a few people along the way
-          too by writing up what i've learned{" "}
-        </p>
-      </div>
-      <div className={styles.imageContainer}>
-        <Image
-          className={styles.heroImage}
-          src="/Images/HomePageImage/profile.JPG"
-          alt="Hero Image"
-          fill
-          objectFit="cover"
-        />
-      </div>
-    </section>
+    <div className={styles.heroContainer}>
+      <section className={styles.hero}>
+        <div className={styles.heroText}>
+          <p>
+            Hey there, I'm Tom, my friends call me Wozza or Tommo though. I'm
+            currently employed as a software engineer, so I thought I'd build
+            this site in my spare time ot help me develop my skills and help me
+            feel more like a software engineer. Maybe help a few people along
+            the way too by writing up what i've learned{" "}
+          </p>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            className={styles.heroImage}
+            src="/Images/HomePageImage/profile.JPG"
+            alt="Hero Image"
+            fill
+            objectFit="cover"
+          />
+        </div>
+      </section>
+    </div>
   );
 };
 
