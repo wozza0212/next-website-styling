661e53bdc503ebfab39c0511bb9b007e45df33da
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2026-04-22 21:35:24 +0100

    hero styling

--- next-website-styling/components/Hero/Hero.tsx
+++ next-website-styling/components/Hero/Hero.tsx
@@ -8,9 +8,11 @@ const HeroSection = () => {
           <p>
             Hey there, I'm Tom, my friends call me Wozza or Tommo though. I'm
             currently employed as a software engineer, so I thought I'd build
-            this site in my spare time ot help me develop my skills and help me
-            feel more like a software engineer. Maybe help a few people along
-            the way too by writing up what i've learned{" "}
+            this site in my spare time to help me develop my skills and feel
+            more like a software engineer. Maybe help a few people along the way
+            too by writing up what i've learned. Over the years I've done a lot
+            of Typescript,React and Python, I also work with AWS, Azure and
+            Terraform as part of an AWS Platform Team.{" "}
           </p>
         </div>
         <div className={styles.imageContainer}>
